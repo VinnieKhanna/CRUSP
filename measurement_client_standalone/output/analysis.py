294294b45c74bf67bba7fea54503b14e7dd8f7f7
Vinnie Khanna <vinniek123@gmail.com>
2023-02-22 16:01:10 -0500

    updating with distance-based measurements

--- measurement_client_standalone/output/analysis.py
+++ measurement_client_standalone/output/analysis.py
@@ -5,15 +5,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 plt.rc("font", size=4)
-files = os.listdir(os.getcwd())
+# files = os.listdir(os.getcwd())
+files = os.listdir("./distance-based")
 fig = plt.figure(dpi=150)
 fig.suptitle("CRUSP Bandiwdth Measurement Results (Mbps)")
 fig.subplots_adjust(hspace=.6, wspace=.6)
 count = 1
 for filename in files:
-    if ".txt" not in filename and ".py" not in filename and "6-23" in filename:  
+    if ".py" not in filename and "6-23" in filename and "iperf" not in filename:  
         rates_list, packets_list = [], []
-        with open(filename, 'r') as f:
+        with open(f"./distance-based/{filename}", 'r') as f:
             for line in f.readlines():
                 [rate, packets] = line.split(", ")
                 rate = rate.split(": ")[1]
@@ -24,9 +25,9 @@ for filename in files:
             #data = np.array([rates_list, packets_list]).T
             rates = np.array(rates_list)
             rates_avg = round(np.average(rates), 2)
-            ax = fig.add_subplot(4, int(len(files)/4), count)
-            count += 2
-            ax.set_title(filename)
+            ax = fig.add_subplot(2, 4, count)
+            count += 1
+            ax.set_title("".join(filename.split("2-6-23-")))
             sns.lineplot(x=np.arange(len(rates_list)), y=rates)
             plt.axhline(rates_avg, linestyle="--")
             plt.text(21, rates_avg, rates_avg)
