0c963d4e8b763f71b93ba91983ecf9f97168977b
Vinnie Khanna <vinniek123@gmail.com>
2023-05-02 00:59:40 -0400

    adding mcs experiment results

--- measurement_client_standalone/run_experiments.sh
+++ measurement_client_standalone/run_experiments.sh
@@ -1,16 +1,16 @@
 # First, make sure we have a very clear iperf3 ground truth bandwidth
 # Use UDP since CRUSP also does
-iperf3 -c 192.168.0.205 -u | grep -e "sender" -e "receiver" > output/mcs/iperf_$1
+iperf3 -c 192.168.1.248 | grep "sender" > output/mcs/iperf_$1
 
 # Second, run a large sequence of CRUSP tests
 # Downlink
-for i in {1..200}
+for i in {1..100}
 do
-	cargo run -- -h 192.168.0.205 -p 8099 | grep -i "naive rate sequence" >> output/mcs/downlink_$1
+	cargo run -- -h 192.168.1.248 -p 8099 | grep -i "naive rate sequence" >> output/mcs/downlink_$1
 done
 
 # Uplink
 for i in {1..100}
 do
-	cargo run -- --uplink -h 192.168.0.205 -p 8099 | grep -i "naive rate sequence" >> output/mcs/uplink_$1
+	cargo run -- --uplink -h 192.168.1.248 -p 8099 | grep -i "naive rate sequence" >> output/mcs/uplink_$1
 done
